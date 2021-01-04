d8f84771d67a482f981b06a0305277c463220cf7
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 00:19:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   lowest_value = name_hash.first[1]
   lowest_key = name_hash.first[0]
   name_hash.each do |key, value|
-    if value < lowest_value
+    if value < lowest_value && hash 
       lowest_value = value
       lowest_key = key
     end
